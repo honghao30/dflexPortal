5a4f82639c1408f3737d48885f65e938732e4e64
honghao30 <58649794+honghao30@users.noreply.github.com>
2023-03-16 17:14:29 +0900

    2 files modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  head: {
+    title: 'Hello Nuxt',
+    htmlAttrs: {
+      lang: 'ko',
+    },
+    meta: [{ charset: 'utf-8' }],
+  },
+});
