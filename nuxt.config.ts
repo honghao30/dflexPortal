58a61c70eb2027b99cf12a377e8d86ef69b50827
honghao30 <58649794+honghao30@users.noreply.github.com>
2023-03-21 17:41:31 +0900

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,16 @@ export default defineNuxtConfig({
   build: {
     transpile: ['vuetify'],
   },
+  head: {
+    title: 'NCPCOGLIB',
+    meta: [
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+    ],
+  },
 });
