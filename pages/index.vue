e999ce02f6c43d36bf637edf0e4e69e334b389bb
honghao30 <58649794+honghao30@users.noreply.github.com>
2023-03-21 16:56:37 +0900

    1 file modified

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,30 @@
       <h1>사이트제목</h1>
     </header>
     <v-sheet class="mx-auto">
-      <v-form @submit.prevent>
+      <v-form class="d-flex" @submit.prevent>
         <v-text-field
           v-model="searchName"
           :rules="rules"
-          label="검색어"
+          variant="outlined"
+          placeholder="검색어"
         ></v-text-field>
-        <v-btn type="submit">검색</v-btn>
+        <v-btn type="submit" variant="flat" color="error">검색</v-btn>
       </v-form>
     </v-sheet>
+    <v-row>
+      <v-list>
+        <v-list-item>키워드1</v-list-item>
+        <v-list-item>키워드2</v-list-item>
+        <v-list-item>키워드3</v-list-item>
+      </v-list>
+    </v-row>
+    <v-row>
+      <v-list>
+        <v-list-item>분류1</v-list-item>
+        <v-list-item>분류2</v-list-item>
+        <v-list-item>분류3</v-list-item>
+      </v-list>
+    </v-row>
   </div>
 </template>
 <script></script>
