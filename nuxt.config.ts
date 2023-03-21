a3e8733b0123e66c71de6a535ae588899eb0c29b
honghao30 <58649794+honghao30@users.noreply.github.com>
2023-03-21 14:04:01 +0900

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  head: {
-    title: 'Hello Nuxt',
-    htmlAttrs: {
-      lang: 'ko',
-    },
-    meta: [{ charset: 'utf-8' }],
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+    transpile: ['vuetify'],
   },
 });
