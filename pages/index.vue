d38bb75a03f0fba1d883e79df6b036a6f3d978aa
honghao30 <58649794+honghao30@users.noreply.github.com>
2023-03-21 17:28:06 +0900

    1 file modified

--- pages/index.vue
+++ pages/index.vue
@@ -33,10 +33,10 @@
           <NuxtLink to="BookList"> 분류1 </NuxtLink>
         </v-list-item>
         <v-list-item>
-          <NuxtLink to="#"> 분류2 </NuxtLink>
+          <NuxtLink to="NoteList"> 분류2 </NuxtLink>
         </v-list-item>
         <v-list-item>
-          <NuxtLink to="#"> 분류3 </NuxtLink>
+          <NuxtLink to="TapaList"> 분류3 </NuxtLink>
         </v-list-item>
       </v-list>
     </v-row>
