30cd36059f3221b8c3599a61acfa95f6f543b73f
honghao30 <58649794+honghao30@users.noreply.github.com>
2023-04-05 16:56:43 +0900

    1 file modified

--- components/common/Quckbar.vue
+++ components/common/Quckbar.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>퀵바</div>
+  <v-bottom-navigation :elevation="1" grow>
+    <v-btn value="recent">
+      <v-icon>mdi-history</v-icon>
+     
+    </v-btn>
+
+    <v-btn value="favorites">
+      <v-icon>mdi-heart</v-icon>
+
+      Favorites
+    </v-btn>
+
+    <v-btn value="nearby">
+      <v-icon>mdi-map-marker</v-icon>
+
+      Nearby
+    </v-btn>
+  </v-bottom-navigation>
 </template>
